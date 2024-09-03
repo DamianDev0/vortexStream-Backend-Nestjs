4342799807380ca16950883d83ba3f493873af32
damian <dg997678@gmail.com>
2024-09-02 20:51:19 -0500

    add config swagger, pipes, connection database postgress, globla prefix and cors

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    // Configuración de ConfigModule para cargar variables de entorno
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Configuración de TypeOrmModule para conectar a la base de datos PostgreSQL
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, //quitar en produccion
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('Bootstrap');
+
+  // Set a global prefix for all routes
+  app.setGlobalPrefix('vortexstream');
+
+  // Configure CORS
+
+  app.enableCors();
+
+  // Configure global pipes for validation and transformation
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('VortexStream API')
+    .setDescription('API documentation for the VortexStream project')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      defaultModelsExpandDepth: -1, // Disable the models section for better readability
+      docExpansion: 'none', // Minimize the operations by default
+    },
+  });
+
+  await app.listen(process.env.PORT || 3000);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
+  logger.log(`Swagger is available at: ${await app.getUrl()}/api-docs`);
+}
+bootstrap();
