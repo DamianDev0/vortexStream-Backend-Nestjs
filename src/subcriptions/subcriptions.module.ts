a312ca68c630a49981e111f78c0d1f118f2c036c
Alejo-develop <yeyito872@gmail.com>
2024-09-16 02:10:07 -0500

    Add finally settings in the subcriptions entity, and logic extra at login

--- src/subcriptions/subcriptions.module.ts
+++ src/subcriptions/subcriptions.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SubcriptionsService } from './subcriptions.service';
 import { SubcriptionsController } from './subcriptions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Subscription } from './entities/subcription.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Subscription, User])],
   controllers: [SubcriptionsController],
   providers: [SubcriptionsService],
+  exports: [SubcriptionsService, TypeOrmModule]
 })
 export class SubcriptionsModule {}
--- src/subcriptions/subcriptions.service.ts
+++ src/subcriptions/subcriptions.service.ts
@@ -1,26 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateSubcriptionDto } from './dto/create-subcription.dto';
 import { UpdateSubcriptionDto } from './dto/update-subcription.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Subscription } from './entities/subcription.entity';
+import { Repository } from 'typeorm';
+import { ActiveUserInterface } from 'src/common/interface/activeUser.interface';
+import { Role } from 'src/common/enum/Roles.enum';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class SubcriptionsService {
-  create(createSubcriptionDto: CreateSubcriptionDto) {
-    return 'This action adds a new subcription';
+  constructor(
+    @InjectRepository(Subscription) private readonly suscriptionRepository: Repository<Subscription>,
+    @InjectRepository(User) private readonly userRepository: Repository<User>
+  ){}
+
+  private validateOwnerShip(subscription: Subscription, user: ActiveUserInterface){
+    if( subscription.userId !== user.id ){
+      throw new UnauthorizedException('You do not have permissions')
+    }
+
+    return subscription
   }
 
-  findAll() {
-    return `This action returns all subcriptions`;
+  private adminGuard(user: ActiveUserInterface){
+    if(user.role !== Role.ADMIN){
+      return false 
+    }
+
+    return true
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} subcription`;
+  async create(userId: string) {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    const newSubscription = this.suscriptionRepository.create({
+      user: user,  
+    });
+
+    return await this.suscriptionRepository.save(newSubscription);
   }
 
-  update(id: number, updateSubcriptionDto: UpdateSubcriptionDto) {
-    return `This action updates a #${id} subcription`;
+  async findAll(user: ActiveUserInterface) {
+    const guard = this.adminGuard(user)
+
+    if(!guard) throw new UnauthorizedException("You don't have permissions")
+
+    return await this.suscriptionRepository.find();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} subcription`;
+  async findOne(id: string, user: ActiveUserInterface) {
+    const subscription = await this.suscriptionRepository.findOne({where: {id}}) 
+
+    if(!subscription) throw new BadRequestException('Subscription not found')
+
+    return this.validateOwnerShip(subscription, user);
+  }
+
+  async update(id: string, updateSubcriptionDto: UpdateSubcriptionDto, user: ActiveUserInterface) {
+    await this.findOne(id, user)
+    
+    return await this.suscriptionRepository.update(id,{
+      ...updateSubcriptionDto
+    });
+  }
+
+  remove(id: string, user: ActiveUserInterface) {
+    if(id !== user.id) throw new UnauthorizedException("You dont have permissions")
+
+    return this.suscriptionRepository.delete(id);
   }
 }
