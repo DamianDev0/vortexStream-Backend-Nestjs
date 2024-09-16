48c0c54e4d24961e327cf02e7570f29ded5c5ab6
Alejo-develop <yeyito872@gmail.com>
2024-09-16 17:11:39 -0500

    little settings in bank and paymthods entitys

--- src/bank/bank.controller.ts
+++ src/bank/bank.controller.ts
@@ -10,12 +10,15 @@ import {
 import { BankService } from './bank.service';
 import { BankDto } from './dtos/create-bank.dto';
 import { UpdateBankDto } from './dtos/update-bank.dto';
+import { AuthDecorator } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/common/enum/Roles.enum';
 
 @Controller('bank')
 export class BankController {
   constructor(private readonly bankService: BankService) {}
 
   // POST /bank - Create a new bank
+  @AuthDecorator(Role.ADMIN)
   @Post()
   async create(@Body() bankDto: BankDto) {
     return await this.bankService.create(bankDto);
@@ -28,30 +31,35 @@ export class BankController {
   }
 
   // GET /bank/:id - Get a single bank by ID
+  @AuthDecorator(Role.ADMIN)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.bankService.findOne(id);
   }
 
   // PATCH /bank/:id - Update a bank by ID
+  @AuthDecorator(Role.ADMIN)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateBankDto: UpdateBankDto) {
     return await this.bankService.update(id, updateBankDto);
   }
 
   // DELETE /bank/:id - Delete a bank by ID
+  @AuthDecorator(Role.ADMIN)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.bankService.remove(id);
   }
 
   // GET /bank/country/:country - Search banks by country
+  @AuthDecorator(Role.ADMIN)
   @Get('/:country')
   async searchByCountry(@Param('country') country: string) {
     return await this.bankService.searchByCountry(country);
   }
 
   // GET /bank/name/:name - Search banks by name
+  @AuthDecorator(Role.ADMIN)
   @Get('/:name')
   async searchByName(@Param('name') name: string) {
     return await this.bankService.searchByName(name);
--- src/pay_method/pay_method.module.ts
+++ src/pay_method/pay_method.module.ts
@@ -3,11 +3,13 @@ import { PayMethodService } from './pay_method.service';
 import { PayMethodController } from './pay_method.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PayMethod } from './entities/pay_method.entity';
+import { Bank } from 'src/bank/entities/banck.entity';
+import { BankService } from 'src/bank/bank.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PayMethod])],
+  imports: [TypeOrmModule.forFeature([PayMethod, Bank])],
   controllers: [PayMethodController],
-  providers: [PayMethodService],
+  providers: [PayMethodService, BankService],
   exports: [PayMethodService], // Para que pueda ser inyectado en otros módulos.  //  En este caso, PayMethodService es exportado para que pueda ser usado en otros módulos.  //  Este módulo tiene un controlador (PayMethodController) y un proveedor (PayMethodService).  //  El proveedor se exporta para que pueda ser usado en otros módulos.
 })
 export class PayMethodModule {}
