a8803ee72f1a0b69f5ac1c80b3d8909375f2ca6c
Alejo-develop <yeyito872@gmail.com>
2024-09-22 01:10:28 -0500

    add settings in history user

--- src/history-user/dto/create-history-user.dto.ts
+++ src/history-user/dto/create-history-user.dto.ts
@@ -1,22 +1,27 @@
-import { IsNumber, IsString, MaxLength } from "class-validator";
+import { IsNumber, IsOptional, IsString, MaxLength } from "class-validator";
 
 export class CreateHistoryUserDto {
+    @IsOptional()
     @IsString()
     userId: string;
 
     @IsString()
     mediaId: string;
 
+    @IsOptional()
     @IsString()
     imgMedia: string;
 
+    @IsOptional()
     @IsString()
     mediaTitle: string;
 
+    @IsOptional()
     @IsString()
     @MaxLength(1200)
     synopsis: string;
 
+    @IsOptional()
     @IsNumber()
-    raiting: number;
+    rating: number;
 }
