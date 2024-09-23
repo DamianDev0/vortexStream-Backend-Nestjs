1dca8c73b28de99b46b717da0a638c3731130712
Alejo-develop <yeyito872@gmail.com>
2024-09-23 03:11:51 -0500

    finally settings in the suscriptions entity

--- src/subcriptions/dto/update-subcription.dto.ts
+++ src/subcriptions/dto/update-subcription.dto.ts
@@ -1,4 +1,12 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSubcriptionDto } from './create-subcription.dto';
+import { IsBoolean, IsString } from "class-validator";
 
-export class UpdateSubcriptionDto extends PartialType(CreateSubcriptionDto) {}
+export class UpdateSubcriptionDto{
+    @IsBoolean()
+    status: boolean
+
+    @IsString()
+    payMethodId: string
+
+    @IsString()
+    duration: string
+}
